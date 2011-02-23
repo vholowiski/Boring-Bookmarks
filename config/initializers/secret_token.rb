2f2e6380f71ad84c9d783256741152273910411f
bitnami <bitnami@domU-12-31-39-14-F9-CF.(none)>
2011-02-23 04:05:14 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmark::Application.config.secret_token = '00e467a7625ff818f5fe53fa9e0dc91b42f9ac194902c9a86e44f8db2b8bc50595774aa9f99cde3787008946a87020ca83c3b156ba8d1880efefd9d23670ef10'
---
+++ db/migrate/20110223035659_create_marks.rb
@@ -0,0 +1,14 @@
+class CreateMarks < ActiveRecord::Migration
+  def self.up
+    create_table :marks do |t|
+      t.string :name
+      t.string :url
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :marks
+  end
+end
