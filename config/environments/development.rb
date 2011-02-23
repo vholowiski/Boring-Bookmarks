3e38130dcb6255384a99f203065f5f54808d1a16
Dave Holowiski <david@holowiski.com>
2011-02-23 04:43:54 +0000

    Installed devise, created User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,9 @@ Bookmark::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  #configure actionmailer for devise
+  #http://www.kiwiluv.com/techblog/?p=397
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
